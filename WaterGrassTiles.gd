60a67619b9a7ed963a540527c3ec6acf54a5dc04
Michael Reel <mykilr@gmail.com>
2017-10-18 23:11:50 +0100

    Changed the tileset a bit, simplified the marching squares algorithm. Still need to re-align and maybe reduce the tree layer horizonally.

--- WaterGrassTiles.gd
+++ WaterGrassTiles.gd
@@ -3,9 +3,10 @@ extends Node2D
 export var gridSize = Vector2()
 export (TileSet) var tileset
 
-const BASE_TILE_MAP = 0
-const SOLID_TILE_MAP = 1
-const PROC_DEBUG_SPRITE = 2
+const WATER_SAND_MAP = 0
+const GRASS_MAP = 1
+const TREE_MAP = 2
+const PROC_DEBUG_SPRITE = 3
 
 var grid_start_x
 var grid_start_y
@@ -25,70 +26,39 @@ func _ready():
 	
 	self.tile_maps = self.get_children()
 	
-	var basemap = tile_maps[BASE_TILE_MAP]
-	var solidmap = tile_maps[SOLID_TILE_MAP]
-	
-	var edgeTool = load("res://MarchingSquares.gd").new(grid_end_x, grid_end_y)
-	
 	# Index tileset groups
 	var water_sand = []
 	for i in range(16):
 		water_sand.append(tileset.find_tile_by_name("WaterSand_%02d" % i))
 	
-	var water_grass = []
+	var grass = []
 	for i in range(16):
-		water_grass.append(tileset.find_tile_by_name("WaterGrass_%02d" % i))
-	
-	var sand_grass = []
-	for i in range(16):
-		sand_grass.append(tileset.find_tile_by_name("SandGrass_%02d" % i))
+		grass.append(tileset.find_tile_by_name("SandGrass_%02d" % i))
 
-	var tree_edge = []
+	var tree = []
 	for i in range(16):
-		tree_edge.append(tileset.find_tile_by_name("Tree_%02d" % i))
+		tree.append(tileset.find_tile_by_name("Tree_%02d" % i))
 	
 	var grasses = []
 	for i in range(1, 9):
 		grasses.append(tileset.find_tile_by_name("Grass_%02d" % i))
 	
-	# Select primary file tiles
-	var water = water_sand[0]
-	var sand = sand_grass[0]
-	var grass = grasses[0]
-	var tree = tree_edge[0]
-	var empty = -1
+	self.tile_width = tileset.tile_get_region(1).size.x
+	self.tile_height = tileset.tile_get_region(1).size.y
 	
-	self.tile_width = tileset.tile_get_region(water).size.x
-	self.tile_height = tileset.tile_get_region(water).size.y
+	basic_perlin_fill(tile_maps, water_sand, grass, tree)
 	
-	# print (self.tile_width, ", ", self.tile_height)
-	# print (water, ", ", grass, ", ", tree, ", ", empty)
-	
-	basic_perlin_fill(tile_maps, grass, sand, water, tree)
-	
-	# edgeTool.simple_marching_squares(basemap, water_grass, grasses)
-	
-	edgeTool.simple_marching_squares(basemap, water_sand, [sand, grass])
-	
-	edgeTool.simple_marching_squares(basemap, sand_grass, [grass])
-	
-	edgeTool.simple_marching_squares(solidmap, tree_edge, [empty], true)
-	
-	randomise_grass(basemap, grass, grasses)
+	randomise_grass(tile_maps[GRASS_MAP], grass[15], grasses)
 
-func basic_perlin_fill(tile_maps, grass, sand, water, tree):
-	
-	var basemap = tile_maps[BASE_TILE_MAP]
-	var solidmap = tile_maps[SOLID_TILE_MAP]
-	var debug = tile_maps[PROC_DEBUG_SPRITE]
+func basic_perlin_fill(tile_maps, water_sand, grass, tree):
 	
 	var perlinRef = load("res://PerlinRef.gd")
 	
 	var base = perlinRef.new(64, 64, 64, 4)
 	var solid = perlinRef.new(64, 64, 64, 7, 20)
 	
-	var image_width = (grid_end_x - grid_start_x) * tile_width
-	var image_height = (grid_end_y - grid_start_y) * tile_height
+	var image_width = (grid_end_x - grid_start_x + 1) * tile_width
+	var image_height = (grid_end_y - grid_start_y + 1) * tile_height
 	
 	var texture = ImageTexture.new()
 	texture.create(image_width, image_height, Image.FORMAT_RGBA)
@@ -102,12 +72,20 @@ func basic_perlin_fill(tile_maps, grass, sand, water, tree):
 	var s1_min = s1_mid
 	var s1_max = s1_mid
 	
-	for tile_y in range(grid_start_y, grid_end_y):
-		for tile_x in range(grid_start_x, grid_end_x):
-			var b1 = base.fractal2d(2, 1, tile_x, tile_y) + b1_mid
-			var s1 = solid.fractal2d(1, 1, tile_x, tile_y) + s1_mid
-			
-			var color = Color(s1, s1, s1)
+	var sand_vertices = []
+	var grass_vertices = []
+	var tree_vertices = []
+	
+	print ("GOT HERE")
+	
+	# Populate simple boolean grids
+	for corner_x in range(grid_start_x, grid_end_x + 1):
+		sand_vertices.append([])
+		grass_vertices.append([])
+		tree_vertices.append([])
+		for corner_y in range(grid_start_y, grid_end_y + 1):
+			var b1 = base.fractal2d(2, 1, corner_x, corner_y) + b1_mid
+			var s1 = solid.fractal2d(1, 1, corner_x, corner_y) + s1_mid
 			
 			b1_min = min(b1_min, b1)
 			b1_max = max(b1_max, b1)
@@ -115,31 +93,74 @@ func basic_perlin_fill(tile_maps, grass, sand, water, tree):
 			s1_min = min(s1_min, s1)
 			s1_max = max(s1_max, s1)
 			
-			# Choose tiles
+			# Base is water, sand is the first cover
 			if b1 <= b1_mid:
-				basemap.set_cell(tile_x, tile_y, water)
-			elif s1 < s1_mid - 0.11:
-				 basemap.set_cell(tile_x, tile_y, sand)
+				sand_vertices[corner_x].append(false)
 			else:
-				basemap.set_cell(tile_x, tile_y, grass)
-				if s1 > s1_mid + 0.11:
-					solidmap.set_cell(tile_x, tile_y, tree)
+				sand_vertices[corner_x].append(true)
+				
+			# Populate grass layer
+			if b1 > b1_mid and s1 > s1_mid - 0.1:
+				grass_vertices[corner_x].append(true)
+			else:
+				grass_vertices[corner_x].append(false)
+			
+			# Populate tree layer - this'll need adjusting, probably
+			if b1 > (b1_mid) and s1 > s1_mid + 0.1:
+				tree_vertices[corner_x].append(true)
+			else:
+				tree_vertices[corner_x].append(false)
 			
 			# Fill debug overlay image
 			var color = Color(b1, b1, b1, 1)
-			var pos_y = tile_y * tile_height
-			var pos_x = tile_x * tile_width
+			var pos_y = corner_y * tile_height
+			var pos_x = corner_x * tile_width
 			
 			for y in range(pos_y, pos_y + tile_height):
 				for x in range(pos_x, pos_x + tile_width):
 					data.put_pixel(x, y, color)
 	
+	
+	for tile_y in range(grid_start_y, grid_end_y):
+		for tile_x in range(grid_start_x, grid_end_x):
+			var sand_score = get_corner_score(sand_vertices, tile_x, tile_y)
+			var grass_score = get_corner_score(grass_vertices, tile_x, tile_y)
+			var tree_score = get_corner_score(tree_vertices, tile_x, tile_y)
+			tile_maps[WATER_SAND_MAP].set_cell(tile_x, tile_y, water_sand[sand_score])
+			if (grass_score > 0):
+				tile_maps[GRASS_MAP].set_cell(tile_x, tile_y, grass[grass_score])
+			if (tree_score > 0):
+				tile_maps[TREE_MAP].set_cell(tile_x, tile_y, tree[tree_score])
+			
+			
+	
 	texture.set_data(data)
-	debug.set_pos(Vector2(image_width / 2, image_height / 2))
-	debug.set_texture(texture)
+	tile_maps[PROC_DEBUG_SPRITE].set_pos(Vector2(image_width / 2, image_height / 2))
+	tile_maps[PROC_DEBUG_SPRITE].set_texture(texture)
 	
 	print ("b1 - min: ", b1_min, ", max: ", b1_max)
 	print ("s1 - min: ", s1_min, ", max: ", s1_max)
+	
+func get_corner_score(grid, x, y, inverse = false):
+	var score = 0
+	
+	# Bottom right
+	if grid[x + 1][y + 1]:
+		score += 1 
+	
+	# Bottom left
+	if grid[x][y + 1]:
+		score += 2
+	
+	# Top right
+	if grid[x + 1][y]:
+		score += 4
+	
+	# Top left
+	if grid[x][y]:
+		score += 8
+	
+	return score
 
 func randomise_grass(tile_map, grass, grasses):
 	for y in range(grid_start_y, grid_end_y):
