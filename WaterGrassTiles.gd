5b6f1ad068d77922051521365bc911192681437f
Michael Reel <mykilr@gmail.com>
2017-10-20 22:28:31 +0100

    Messing about somewhat with the debug sprite, normalising the colors a bit.

--- WaterGrassTiles.gd
+++ WaterGrassTiles.gd
@@ -8,11 +8,19 @@ const GRASS_MAP = 1
 const TREE_MAP = 2
 const PROC_DEBUG_SPRITE = 3
 
+const wave_width = 32
+const wave_height = 32
+const wave_depth = 1
+
 var grid_start_x
 var grid_start_y
 var grid_end_x
 var grid_end_y
-var tile_maps
+
+var water_sand_map
+var grass_map
+var tree_map
+var debug_sprite
 
 var tile_width
 var tile_height
@@ -24,7 +32,10 @@ func _ready():
 	self.grid_end_x = int(gridSize.x)
 	self.grid_end_y = int(gridSize.y)
 	
-	self.tile_maps = self.get_children()
+	self.water_sand_map = self.find_node("WaterSand", false)
+	self.grass_map = self.find_node("Grass", false)
+	self.tree_map = self.find_node("Tree", false)
+	self.debug_sprite = self.find_node("ProcDebug", false)
 	
 	# Index tileset groups
 	var water_sand = []
@@ -46,16 +57,17 @@ func _ready():
 	self.tile_width = tileset.tile_get_region(1).size.x
 	self.tile_height = tileset.tile_get_region(1).size.y
 	
-	basic_perlin_fill(tile_maps, water_sand, grass, tree)
+	basic_perlin_fill(water_sand, grass, tree)
 	
-	randomise_grass(tile_maps[GRASS_MAP], grass[15], grasses)
+	randomise_grass(self.grass_map, grass[15], grasses)
 
-func basic_perlin_fill(tile_maps, water_sand, grass, tree):
+func basic_perlin_fill(water_sand, grass, tree):
 	
 	var perlinRef = load("res://PerlinRef.gd")
 	
-	var base = perlinRef.new(64, 64, 64, 4)
-	var solid = perlinRef.new(64, 64, 64, 7, 20)
+	var base = perlinRef.new(wave_width, wave_height, wave_depth, 4)
+	var solid = perlinRef.new(wave_width, wave_height, wave_depth, 7, 20)
+	var rename_this = perlinRef.new(wave_width, wave_height, wave_depth, 13, 1023)
 	
 	var image_width = (grid_end_x - grid_start_x + 1) * tile_width
 	var image_height = (grid_end_y - grid_start_y + 1) * tile_height
@@ -64,7 +76,7 @@ func basic_perlin_fill(tile_maps, water_sand, grass, tree):
 	texture.create(image_width, image_height, Image.FORMAT_RGBA)
 	var data = Image(image_width, image_height, false, Image.FORMAT_RGBA)
 	
-	var b1_mid = 0.5
+	var b1_mid = 0
 	var b1_min = b1_mid
 	var b1_max = b1_mid
 	
@@ -72,11 +84,13 @@ func basic_perlin_fill(tile_maps, water_sand, grass, tree):
 	var s1_min = s1_mid
 	var s1_max = s1_mid
 	
+	var t1_mid = 0
+	var t1_min = t1_mid
+	var t1_max = t1_mid
+	
 	var sand_vertices = []
 	var grass_vertices = []
 	var tree_vertices = []
-	
-	print ("GOT HERE")
 	
 	# Populate simple boolean grids
 	for corner_x in range(grid_start_x, grid_end_x + 1):
@@ -84,80 +98,74 @@ func basic_perlin_fill(tile_maps, water_sand, grass, tree):
 		grass_vertices.append([])
 		tree_vertices.append([])
 		for corner_y in range(grid_start_y, grid_end_y + 1):
-			var b1 = base.fractal2d(2, 1, corner_x, corner_y) + b1_mid
-			var s1 = solid.fractal2d(1, 1, corner_x, corner_y) + s1_mid
+			var b1 = base.fractal2d(1, 1, corner_x, corner_y)
+			var s1 = solid.fractal2d(1, 1, corner_x, corner_y)
+			var t1 = rename_this.fractal2d(1, 1, corner_x, corner_y)
 			
 			b1_min = min(b1_min, b1)
 			b1_max = max(b1_max, b1)
+			sand_vertices[corner_x].append(b1)
 			
 			s1_min = min(s1_min, s1)
 			s1_max = max(s1_max, s1)
+			grass_vertices[corner_x].append(min(s1, b1))
 			
-			# Base is water, sand is the first cover
-			if b1 <= b1_mid:
-				sand_vertices[corner_x].append(false)
-			else:
-				sand_vertices[corner_x].append(true)
-				
-			# Populate grass layer
-			if b1 > b1_mid and s1 > s1_mid - 0.1:
-				grass_vertices[corner_x].append(true)
-			else:
-				grass_vertices[corner_x].append(false)
+			t1_min = min(t1_min, t1)
+			t1_max = max(t1_max, t1)
+			tree_vertices[corner_x].append(min(t1, min(s1, b1)))
 			
-			# Populate tree layer - this'll need adjusting, probably
-			if b1 > (b1_mid) and s1 > s1_mid + 0.1:
-				tree_vertices[corner_x].append(true)
-			else:
-				tree_vertices[corner_x].append(false)
+	var b_sparsity = b1_mid
+	var s_sparsity = s1_mid
+	var t_sparsity = t1_mid + 0.15
+	
+	for tile_y in range(grid_start_y, grid_end_y):
+		for tile_x in range(grid_start_x, grid_end_x):
+			var sand_score = get_corner_score(sand_vertices, b_sparsity, tile_x, tile_y)
+			var grass_score = get_corner_score(grass_vertices, s_sparsity, tile_x, tile_y)
+			var tree_score = get_corner_score(tree_vertices, t_sparsity, tile_x, tile_y)
+			
+			self.water_sand_map.set_cell(tile_x, tile_y, water_sand[sand_score])
+			if (grass_score > 0):
+				self.grass_map.set_cell(tile_x, tile_y, grass[grass_score])
+			if (tree_score > 0):
+				self.tree_map.set_cell(tile_x, tile_y, tree[tree_score])
 			
 			# Fill debug overlay image
-			var color = Color(b1, b1, b1, 1)
-			var pos_y = corner_y * tile_height
-			var pos_x = corner_x * tile_width
+			var shade = sand_vertices[tile_x + 1][tile_y + 1]
+			shade = (shade - b1_min) / (b1_max -  b1_min)
+			var color =  Color(shade, shade, shade)
+			
+			var pos_y = tile_y * tile_height
+			var pos_x = tile_x * tile_width
 			
 			for y in range(pos_y, pos_y + tile_height):
 				for x in range(pos_x, pos_x + tile_width):
 					data.put_pixel(x, y, color)
 	
-	
-	for tile_y in range(grid_start_y, grid_end_y):
-		for tile_x in range(grid_start_x, grid_end_x):
-			var sand_score = get_corner_score(sand_vertices, tile_x, tile_y)
-			var grass_score = get_corner_score(grass_vertices, tile_x, tile_y)
-			var tree_score = get_corner_score(tree_vertices, tile_x, tile_y)
-			tile_maps[WATER_SAND_MAP].set_cell(tile_x, tile_y, water_sand[sand_score])
-			if (grass_score > 0):
-				tile_maps[GRASS_MAP].set_cell(tile_x, tile_y, grass[grass_score])
-			if (tree_score > 0):
-				tile_maps[TREE_MAP].set_cell(tile_x, tile_y, tree[tree_score])
-			
-			
-	
 	texture.set_data(data)
-	tile_maps[PROC_DEBUG_SPRITE].set_pos(Vector2(image_width / 2, image_height / 2))
-	tile_maps[PROC_DEBUG_SPRITE].set_texture(texture)
+	self.debug_sprite.set_pos(Vector2(image_width / 2, image_height / 2))
+	self.debug_sprite.set_texture(texture)
 	
 	print ("b1 - min: ", b1_min, ", max: ", b1_max)
 	print ("s1 - min: ", s1_min, ", max: ", s1_max)
 	
-func get_corner_score(grid, x, y, inverse = false):
+func get_corner_score(grid, limit, x, y):
 	var score = 0
 	
 	# Bottom right
-	if grid[x + 1][y + 1]:
+	if grid[x + 1][y + 1] > limit:
 		score += 1 
 	
 	# Bottom left
-	if grid[x][y + 1]:
+	if grid[x][y + 1] > limit:
 		score += 2
 	
 	# Top right
-	if grid[x + 1][y]:
+	if grid[x + 1][y] > limit:
 		score += 4
 	
 	# Top left
-	if grid[x][y]:
+	if grid[x][y] > limit:
 		score += 8
 	
 	return score
